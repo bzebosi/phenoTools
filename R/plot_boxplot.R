4c5415fb53d2799e7ee98cf29c18dbeabfc38d44
bzebosi <zebosibrian@gmail.com>
2026-08-21 07:28:26 -0700

    plot update

--- R/plot_boxplot.R
+++ R/plot_boxplot.R
@@ -2,9 +2,9 @@ plot_boxplot <- function(
     data, x_col = "Genotype", y_col = "Value",
     fill_col = x_col, y_label = NULL, 
     x_label = NULL, title = NULL, y_limits = NULL, colors = NULL, 
-    box_alpha = 0.8, box_fill = TRUE, box_linewidth = 1, box_width = 0.6,
-    jitter_size = 3, jitter_width = 0.2, jitter_shape = 16, 
-    dodge_width = 0.8, outlier_shape = NA, show_points = TRUE,
+    box_alpha = 1, box_fill = TRUE, box_linewidth = 1, box_width = 0.55,
+    jitter_size = 2.5, jitter_width = 0.12, jitter_shape = 16, jitter_alpha = 0.80,
+    dodge_width = 0.8, outlier_shape = NA, show_points = TRUE, flip = FALSE,
     ...
 ) {
   
@@ -13,12 +13,11 @@ plot_boxplot <- function(
   
   if (grouped_plot) {
     box_position <- ggplot2::position_dodge(width = dodge_width)
-    
     point_position <- ggplot2::position_jitterdodge(jitter.width = jitter_width,
                                                     dodge.width = dodge_width)
   } else {
     box_position <- "identity"
-    point_position <- ggplot2::position_jitter(width = jitter_width, height = 0)
+    point_position <- ggplot2::position_jitter(width = jitter_width)
   }
   
   # Create the base boxplot
@@ -48,13 +47,15 @@ plot_boxplot <- function(
   # Add jittered observations
   if (show_points) {
     p <- p + ggplot2::geom_point(shape = jitter_shape, position = point_position,
-                                 size = jitter_size)
-    }
+                                 alpha = jitter_alpha, size = jitter_size)
+  }
   
-  # Set the visible y-axis range
-  if (!is.null(y_limits)) { 
-    p <- p + ggplot2::coord_cartesian(ylim = y_limits) 
-    }
+  # Set coordinate system and visible y-axis range
+  if (flip) {
+    p <- p + ggplot2::coord_flip(ylim = y_limits)
+  } else if (!is.null(y_limits)) {
+    p <- p + ggplot2::coord_cartesian(ylim = y_limits)
+  }
   
   # Add plot labels and apply the custom theme
   p + ggplot2::labs(title = title, x = x_label, y = y_label) +
